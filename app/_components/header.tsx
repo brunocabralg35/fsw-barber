dba754c599f4ff84a6dcfec62612956678cd7237
Bruno Cabral <brunocabral035@gmail.com>
2024-02-05 18:49:37 -0300

    feat: OAuth integration

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
   return (
     <Card>
       <CardContent className="p-5 justify-between flex items-center">
         <Image src="/logo.png" alt="FSW Barber" height={18} width={130} />
-        <Button variant="outline" size="icon" className="h-8 w-8">
+        {/* <Button variant="outline" size="icon" className="h-8 w-8">
           <MenuIcon size={18} />
-        </Button>
+        </Button> */}
+        {data?.user ? (
+          <h1>{data.user.name}</h1>
+        ) : (
+          <Button onClick={handleLoginClick}>Login</Button>
+        )}
       </CardContent>
     </Card>
   );
