31ff616fd4cad7fa59f3d4a7ba68428cfb86b08d
Bruno Cabral <brunocabral035@gmail.com>
2024-02-06 11:04:55 -0300

    fix: Oauth callback error

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,27 +4,23 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
   const { data } = useSession();
 
   const handleLoginClick = async () => {
-    await signIn();
+    await signIn("google");
   };
 
   return (
     <Card>
       <CardContent className="p-5 justify-between flex items-center">
         <Image src="/logo.png" alt="FSW Barber" height={18} width={130} />
-        {/* <Button variant="outline" size="icon" className="h-8 w-8">
+        <Button variant="outline" size="icon" className="h-8 w-8">
           <MenuIcon size={18} />
-        </Button> */}
-        {data?.user ? (
-          <h1>{data.user.name}</h1>
-        ) : (
-          <Button onClick={handleLoginClick}>Login</Button>
-        )}
+        </Button>
+        
       </CardContent>
     </Card>
   );
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,10 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
+
 import { db } from "@/app/_lib/prisma";
+
+
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
@@ -11,7 +14,7 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-  ],
+  ]
 };
 
 const handler = NextAuth(authOptions);
