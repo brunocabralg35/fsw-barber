ece637cdda18ae4b61c0ec5e937bb69665bd39b8
Bruno Cabral <brunocabral035@gmail.com>
2024-02-08 15:41:31 -0300

    refactor: add bookingInfo to show booking information

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -23,6 +23,7 @@ import { Loader2Icon } from "lucide-react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import getDayBookings from "../_actions/get-day-bookings";
+import BookingInfo from "@/app/_components/booking-info";
 
 interface ServiceItemProps {
   barbershop: Barbershop;
@@ -124,8 +125,7 @@ const ServiceItem = ({
   };
 
   return (
-    <div>
-      <Card>
+      <Card className="w-full overflow-hidden">
         <CardContent className="p-3">
           <div className="flex gap-4 items-center">
             <div className="relative min-h-[110px] max-h-[110px] min-w-[110px] max-w-[110px]">
@@ -138,7 +138,9 @@ const ServiceItem = ({
             </div>
             <div className="flex flex-col w-full">
               <h2 className="font-bold">{service.name}</h2>
-              <p className="text-sm text-gray-400">{service.description}</p>
+              <p className="text-sm text-gray-400 text-ellipsis text-wrap overflow-hidden">
+                {service.description}
+              </p>
 
               <div className="flex items-center justify-between mt-3">
                 <p className="font-bold text-sm text-primary">
@@ -209,43 +211,20 @@ const ServiceItem = ({
                       </div>
                     )}
 
-                    <div className="py-6 px-5 border-t border-solid border-secondary">
-                      <Card>
-                        <CardContent className="p-3 gap-2 flex flex-col">
-                          <div className="flex justify-between items-center">
-                            <h2 className="font-bold">{service.name}</h2>
-                            <h3 className="font-bold text-sm">
-                              {Intl.NumberFormat("pt-BR", {
-                                style: "currency",
-                                currency: "BRL",
-                              }).format(Number(service.price))}
-                            </h3>
-                          </div>
-
-                          {date && (
-                            <div className="flex justify-between items-center">
-                              <h3 className="text-gray-400 text-sm">Data</h3>
-                              <h4 className="text-sm">
-                                {format(date, "dd 'de' MMMM", {
-                                  locale: ptBR,
-                                })}
-                              </h4>
-                            </div>
-                          )}
-
-                          {hour && (
-                            <div className="flex justify-between items-center">
-                              <h3 className="text-gray-400 text-sm">Horário</h3>
-                              <h4 className="text-sm">{hour}</h4>
-                            </div>
-                          )}
-
-                          <div className="flex justify-between items-center">
-                            <h3 className="text-gray-400 text-sm">Barbearia</h3>
-                            <h4 className="text-sm">{barbershop.name}</h4>
-                          </div>
-                        </CardContent>
-                      </Card>
+                    <div className="px-5 mb-5">
+                      <BookingInfo
+                        booking={{
+                          barbershop: barbershop,
+                          date:
+                            date && hour
+                              ? setMinutes(
+                                  setHours(date, Number(hour.split(":")[0])),
+                                  Number(hour.split(":")[1])
+                                )
+                              : undefined,
+                          service: service,
+                        }}
+                      />
                     </div>
 
                     <SheetFooter className="px-5">
@@ -266,7 +245,6 @@ const ServiceItem = ({
           </div>
         </CardContent>
       </Card>
-    </div>
   );
 };
 
